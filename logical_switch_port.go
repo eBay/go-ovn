b93fb4427fb847e792d25400b8c0a7ba0839a505
Aniket <anbhat@redhat.com>
2020-04-11 07:21:02 -0400

    Remove spurious dynamic address type assertion
    There was a left-over type assertion after changes were made to
    do a switch for the contained type of the interface. This commit
    fixes that.

--- logical_switch_port.go
+++ logical_switch_port.go
@@ -331,7 +331,6 @@ func (odbi *ovndb) rowToLogicalPort(uuid string) *LogicalSwitchPort {
 		default:
 			//	glog.V(OVNLOGLEVEL).Info("Unsupport type found in lport dynamic address.")
 		}
-		lp.DynamicAddresses = dynamicAddresses.(string)
 	}
 
 	return lp
